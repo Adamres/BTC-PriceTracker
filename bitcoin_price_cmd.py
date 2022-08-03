2f5f72783fe4b235529a1bbdbf7d761531e408ce
Adamres <41522712+Adamres@users.noreply.github.com>
2022-08-03 20:40:30 +0200

    Add files via upload

---
+++ bitcoin_price_cmd.py
@@ -0,0 +1,13 @@
+import requests
+import time
+from colorama import init, Fore, Back, Style
+
+while True:
+    
+    response = requests.get('https://api.coindesk.com/v1/bpi/currentprice.json')
+    data = response.json()
+    init(autoreset = True)
+    print("\n\n\n"+str(50*"-")+"\nBitcoin current price is: "+Style.BRIGHT + Fore.RED+str(data["bpi"]["USD"]["rate"])+Style.RESET_ALL+" USD.\n"+str(50*"-"))
+    print("\n\n" +Style.BRIGHT + Fore.MAGENTA+ str(10*" ")+"Refresh in every minute..\n\n\n")
+
+    time.sleep(60)
